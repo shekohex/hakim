276b1cdca11a47a4aae3d30adac05ed44b777019
Shady Khalifa <dev+github@shadykhalifa.me>
2026-02-16 22:40:16 +0200

    fix(lazyvim): ensure coder owns config dir before clone

--- devcontainers/.devcontainer/features/src/lazyvim/install.sh
+++ devcontainers/.devcontainer/features/src/lazyvim/install.sh
@@ -16,6 +16,9 @@ fi
 USER_HOME=$(eval echo ~"${_REMOTE_USER}")
 NVIM_CONFIG_DIR="${USER_HOME}/.config/nvim"
 
+mkdir -p "${USER_HOME}/.config"
+chown -R "${_REMOTE_USER}":"${_REMOTE_USER}" "${USER_HOME}/.config"
+
 if [ -d "${NVIM_CONFIG_DIR}" ]; then
     echo "Neovim config already exists at ${NVIM_CONFIG_DIR}, skipping..."
     exit 0
@@ -23,7 +26,7 @@ fi
 
 echo "Installing LazyVim for ${_REMOTE_USER}..."
 
-su - "${_REMOTE_USER}" -c "git clone --depth 1 ${LAZYVIM_REPO} ${NVIM_CONFIG_DIR}"
-su - "${_REMOTE_USER}" -c "rm -rf ${NVIM_CONFIG_DIR}/.git"
+su "${_REMOTE_USER}" -s /bin/bash -c "HOME=${USER_HOME} PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin git clone --depth 1 ${LAZYVIM_REPO} ${NVIM_CONFIG_DIR}"
+su "${_REMOTE_USER}" -s /bin/bash -c "HOME=${USER_HOME} rm -rf ${NVIM_CONFIG_DIR}/.git"
 
 echo "LazyVim installed successfully."
